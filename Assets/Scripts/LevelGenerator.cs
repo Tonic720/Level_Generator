d3dc8626e30e3fc44535330656456cd6a26c7ef7
Unknown <tonicwater720@gmail.com>
2017-07-27 10:00:18 -0400

    Working level geneator

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LevelGenerator : MonoBehaviour {
+	public GameObject[] tiles;
+	public GameObject wall;
+	public int test = 0;
+
+
+	public float chanceUp;
+	public float chanceRight;
+	public float chanceLeft;
+	public int counter = 0;
+
+	public LevelGenerator otherGen;
+	public LevelGenerator otherGen2;
+
+
+		
+
+
+	public List<Vector3> createdTiles;
+
+	public int tileAmount;
+	public int tileOffset;
+
+	public float waitTime;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (GenerateLevel());
+
+		// if you want to create the same level every time use this 
+		//Random.seed = 10;
+
+
+	}
+
+
+	IEnumerator GenerateLevel(){
+
+
+		for(int i = 0; i < tileAmount; i++){
+			
+			float dir = Random.Range(0f,1f);
+			int tile = Random.Range (0, tiles.Length);
+
+			CallMoveGen (dir);
+			CreateTile (tile);
+
+			yield return new WaitForSeconds (waitTime);
+			
+		}
+
+		yield return 0;
+	}
+
+	void CallMoveGen(float ranDir){
+
+		if (ranDir < chanceUp) {
+			MoveGen (0);
+		} else if (ranDir < chanceRight) {
+			MoveGen (1);
+		} else if (ranDir < chanceLeft) {
+			MoveGen (2);
+		} else {
+			MoveGen (3);
+		}
+	}
+
+	void MoveGen(int dir){
+
+
+		switch(dir){
+
+		case 0:
+			transform.position = new Vector3 (transform.position.x, transform.position.y + tileOffset, 0);
+			break;
+		
+		
+		case 1:
+			transform.position = new Vector3 (transform.position.x + tileOffset, transform.position.y, 0);
+			break;
+
+		case 2:
+			transform.position = new Vector3 (transform.position.x, transform.position.y - tileOffset, 0);
+			break;
+
+		case 3:
+			transform.position = new Vector3 (transform.position.x - tileOffset, transform.position.y, 0);
+			break;
+
+
+
+
+
+
+
+		}
+
+
+	}
+
+	void CreateTile(int tileIndex){
+
+		if (!createdTiles.Contains (transform.position) && !otherGen.createdTiles.Contains(transform.position)
+			&& !otherGen2.createdTiles.Contains(transform.position)) {
+			GameObject tileObject;
+
+			tileObject = Instantiate (tiles [tileIndex], transform.position, transform.rotation) as GameObject;
+
+			createdTiles.Add (tileObject.transform.position);
+			counter++;
+
+			
+		}else{
+			tileAmount++;
+		}
+
+
+	}
+	
+
+}
---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Restart : MonoBehaviour {
+
+	public LevelGenerator gen1;
+	public LevelGenerator gen2;
+	public LevelGenerator gen3;
+	public Text text;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gen1.counter == 75 && gen2.counter == 75 && gen3.counter == 75) {
+			//EditorSceneManager.LoadScene ("Generator");
+			text.text = "PRESS R to Restart";
+
+			if(Input.GetKey(KeyCode.R)){
+				
+				SceneManager.LoadScene("Generator");
+			}
+		}
+	}
+}
